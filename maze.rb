22f40b7a69b65ddbe8a3df25ae6ff0d0227544f5
Jakub Pastuszek <jpastuszek@newbay.com>
2010-10-28 17:09:42 +0100

    maze generation works

--- maze.rb
+++ maze.rb
@@ -3,7 +3,7 @@
 class Side
   class Cell
     def initialize
-      @open = false
+      @carved = nil
 
       @left = nil
       @right = nil
@@ -16,17 +16,47 @@ class Side
     attr_accessor :up
     attr_accessor :down
 
-    def open?
-      return @open
+    def carved?
+      return @carved
     end
 
-    def open
-      @open = true
+    def carve
+      @carved = true
       self
     end
 
+    def unmade?
+      puts "unmade?"
+      p self
+
+      return true if sourrunded_by >= 3
+      return nil
+    end
+
+    def sourrunded_by
+      no = 0
+      no += 1 if @left and not @left.carved?
+      no += 1 if @right and not @right.carved?
+      no += 1 if @up and not @up.carved?
+      no += 1 if @down and not @down.carved?
+
+      no += 1 unless @left
+      no += 1 unless @right
+      no += 1 unless @up
+      no += 1 unless @down
+      return no
+    end
+
+    def inspect
+      out = "Cell (sourrunded_by #{sourrunded_by} walls):\n"
+      out << "  #{up and up.to_s or "00"}  \n"
+      out << "#{left and left.to_s or "00"}#{to_s}#{right and right.to_s or "00"}\n"
+      out << "  #{down and down.to_s or "00"}  \n"
+      out
+    end
+
     def to_s
-      return "  " if open? 
+      return "  " if carved? 
       return "[]"
     end
   end
@@ -82,7 +112,76 @@ class Side
   end
 end
 
-s1 = Side.new(1, 7)
+class RecursiveBacktracker
+  class Dirs
+    def initialize
+      @dirs = [:left, :right, :up, :down]
+    end
+
+    def rand
+      return nil if @dirs.empty?
+      dir = @dirs[Kernel.rand(@dirs.length)]
+      @dirs.delete(dir)
+      dir
+    end
+
+    def to_s
+      p @dirs
+    end
+  end
+
+  def initialize(cell)
+    stack = []
+
+    cell.carve
+    stack << cell
+
+    until stack.empty?
+      loop do
+        next_cell = find_move(cell) or break
+
+        next_cell.carve
+        stack << cell
+
+        cell = next_cell
+      end
+
+      cell = stack.pop
+    end
+  end
+
+  def find_move(cell)
+    dirs = Dirs.new
+    puts "finding move"
+    loop do
+      dir = dirs.rand or break
+
+      puts "trying dir: #{dir}"
+
+      c = cell.send(dir)
+      puts "c: '#{c}'"
+      next unless c
+
+      puts "carved? #{c.carved?.inspect}"
+      next if c.carved?
+
+      puts "unmade? #{c.unmade?.inspect}"
+      next unless c.unmade?
+
+      puts "found move to #{c}"
+      return c
+    end
+
+    puts "move not found"
+    return nil
+  end
+
+  def cell_in(dir)
+    c.send(dir)
+  end
+end
+
+s1 = Side.new(1, 40)
 
 puts s1
 
@@ -90,7 +189,10 @@ puts s1.cell(10,0)
 puts s1.cell(0,10)
 puts s1.cell(0,0)
 
-puts s1.cell(0,0).open.right.open.right.open.down.open.right.open
+begin
+  RecursiveBacktracker.new(s1.cell(3, 0))
+ensure
+  puts s1
+end
 
-puts s1
 
